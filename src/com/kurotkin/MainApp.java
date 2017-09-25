27287ff81554f7f51de26aa35c1da5aedb70d71e
kurotkin <kurotkin@users.noreply.github.com>
2017-09-25 16:47:46 +0300

    Added controllers

---
+++ src/com/kurotkin/MainApp.java
@@ -0,0 +1,89 @@
+package com.kurotkin;
+/**
+ * Created by Vitaly Kurotkin on 25.09.2017.
+ */
+
+import com.kurotkin.model.Person;
+import com.kurotkin.view.PersonOverviewController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Observable;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    public MainApp(){
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        try {
+            initRootLayout();
+            showPersonOverview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+    public void initRootLayout() throws IOException {
+        // Загружаем корневой макет из fxml файла.
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/RootLayout.fxml"));
+        rootLayout = (BorderPane) loader.load();
+
+        // Отображаем сцену, содержащую корневой макет.
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void showPersonOverview() throws IOException {
+        // Загружаем сведения об адресатах.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+        AnchorPane personOverview = (AnchorPane) loader.load();
+
+        // Помещаем сведения об адресатах в центр корневого макета.
+        rootLayout.setCenter(personOverview);
+
+        // Даём контроллеру доступ к главному приложению.
+        PersonOverviewController controller = loader.getController();
+        controller.setMainApp(this);
+    }
+}
---
+++ src/com/kurotkin/view/PersonOverviewController.java
@@ -0,0 +1,97 @@
+package com.kurotkin.view;
+
+import com.kurotkin.MainApp;
+import com.kurotkin.model.Person;
+import com.kurotkin.сontroller.util.DateUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * Created by Vitaly Kurotkin on 25.09.2017.
+ */
+public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    private MainApp mainApp;
+
+    public PersonOverviewController() {
+    }
+
+    @FXML
+    private void initialize() {
+        // Инициализация таблицы адресатов с двумя столбцами.
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+
+        // Очистка дополнительной информации об адресате.
+        showPersonDetails(null);
+
+        // Слушаем изменения выбора, и при изменении отображаем дополнительную информацию об адресате.
+        personTable.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> showPersonDetails(newValue));
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+        personTable.setItems(mainApp.getPersonData());
+    }
+
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            // Заполняем метки информацией из объекта person.
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            streetLabel.setText(person.getStreet());
+            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+            cityLabel.setText(person.getCity());
+            birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+        } else {
+            // Если Person = null, то убираем весь текст.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+        }
+    }
+
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            personTable.getItems().remove(selectedIndex);
+        } else {
+            // Ничего не выбрано.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+
+            alert.showAndWait();
+        }
+    }
+}
